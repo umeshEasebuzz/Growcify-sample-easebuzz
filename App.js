f6070aea7b24b87a4b729be7c29d24beb3872f24
imromec <iamromec@gmail.com>
2023-11-23 12:53:00 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Button,
+  NativeModules,
+  NativeEventEmitter,
+} from "react-native";
+import EasebuzzCheckout from "react-native-easebuzz-kit";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open + App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// npm install $(npm pack /Users/maggie/Downloads/paywitheasebuzz-react-native-lib-master/paywitheasebuzz-react-native-lib/react-native-easebuzz-kit | tail -1)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
