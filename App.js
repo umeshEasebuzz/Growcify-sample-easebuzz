4aad94885cf4a9a4dc289561769612fef74acc2a
Umesh Pedde <umesh.pedde@easebuzz.in>
2023-12-04 12:07:42 +0530

    Added sample app with easebuzz kit integration.

--- App.js
+++ App.js
@@ -11,10 +11,37 @@ import {
 import EasebuzzCheckout from "react-native-easebuzz-kit";
 
 export default function App() {
+  const callPaymentGateway = () => {
+    var options = {
+      access_key:
+        '77119d6dbad5c65f9323558e5a6ff4542d7c831b733a95cbc9225f6929ea2c38',
+      pay_mode: 'production',
+    };
+
+    EasebuzzCheckout.open(options)
+      .then(data => {
+        //handle the payment success & failed response here
+        console.log('Payment Response:');
+        console.log(data);
+      })
+      .catch(error => {
+        //handle sdk failure issue here
+        console.log('SDK Error:'
+);
+        console.log(error);
+      });
+  };
   return (
     <View style={styles.container}>
       <Text>Open + App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Click Me"
+        onPress={() => {
+          callPaymentGateway();
+          console.log('hi');
+        }}
+      />
     </View>
   );
 }
